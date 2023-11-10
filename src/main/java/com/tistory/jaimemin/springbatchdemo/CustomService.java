50b83234b9fef3614ee6a774db2837e285e9b730
jaimemin <jaimemin@gmail.com>
2023-11-11 04:12:26 +0900

    implemented sample JobExplorer example

--- src/main/java/com/tistory/jaimemin/springbatchdemo/CustomService.java
+++
@@ -1,8 +0,0 @@
-package com.tistory.jaimemin.springbatchdemo;
-
-public class CustomService {
-
-    public void serviceMethod(String message) {
-        System.out.println(message);
-    }
-}
--- src/main/java/com/tistory/jaimemin/springbatchdemo/SpringBatchDemoApplication.java
+++ src/main/java/com/tistory/jaimemin/springbatchdemo/SpringBatchDemoApplication.java
@@ -1,12 +1,48 @@
 package com.tistory.jaimemin.springbatchdemo;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchDemoApplication {
+
+    @Autowired
+    private JobExplorer jobExplorer;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job explorerJob() {
+        return this.jobBuilderFactory.get("explorerJob")
+                .start(explorerStep())
+                .build();
+    }
+
+    @Bean
+    public Step explorerStep() {
+        return this.stepBuilderFactory.get("explorerStep")
+                .tasklet(exploringTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet exploringTasklet() {
+        return new ExploringTasklet(jobExplorer);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchDemoApplication.class, args);
